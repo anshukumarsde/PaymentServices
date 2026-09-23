80529e055d4282af998330547c85a64e2d465a1d
Anshu Kumar <akumar685@gatech.edu>
2026-09-23 15:32:35 -0400

    moved PaymentController to its separate folder

--- src/main/java/com/example/payments/controller/PaymentController.java
+++ src/main/java/com/example/payments/controller/PaymentController.java
@@ -1,4 +1,4 @@
-package com.example.payments;
+package com.example.payments.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
